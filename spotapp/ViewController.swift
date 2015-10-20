b056a087ca0a24d4f9be62a3d57e782af7fc2f5e
XuebinZhao <xzhao00@citymail.cuny.edu>
2015-10-20 19:37:48 -0400

    Initial Commit

---
+++ spotapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  spotapp
+//
+//  Created by xbin on 10/20/15.
+//  Copyright © 2015 TeamSpot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
